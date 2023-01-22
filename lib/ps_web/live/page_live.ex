3188be2192dba53d153518c4e47365b2d3a5c6cd
Zen <ztang230@gmail.com>
2023-01-21 20:57:27 -0600

    the danger zone: user style..

--- lib/ps_web/live/page_live.ex
+++ lib/ps_web/live/page_live.ex
@@ -2,7 +2,12 @@ defmodule PsWeb.PageLive do
   use PsWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, count: 0, markdown_blurb: "
+    {:ok, assign(socket, count: 0, user_style: "
+p:hover {
+  color: firebrick !important;
+}
+",
+    markdown_blurb: "
 hi
 
 # What's up homies
@@ -17,7 +22,7 @@ here is a paragaph
 
 here is a [link to google](http://google.com)
 
-
+Time went by and not enough vendors implemented the new feature (Firefox and Chrome included some experimental support), so it was eventually dropped. The browser behaviour remains as it was before HTML 5, but the current spec at least documents it: style is now legal/valid throughout the document, but the spec warns about the potential side effects (restyling elements accidentally).
     ")}
   end
 
