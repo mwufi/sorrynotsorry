94498862fbff4bf5c44ec17cc34c99ded836b167
Zen <ztang230@gmail.com>
2023-01-29 16:56:23 -0600

    empty profile page

--- lib/ps_web/live/profile_live/show.ex
+++ lib/ps_web/live/profile_live/show.ex
@@ -2,7 +2,7 @@ defmodule PsWeb.ProfileLive.Show do
   use PsWeb, :live_view
 
   alias Ps.Profiles
-  on_mount {PsWeb.UserAuth, :mount_current_user}
+  on_mount({PsWeb.UserAuth, :mount_current_user})
 
   @impl true
   def mount(_params, _session, socket) do
@@ -11,12 +11,26 @@ defmodule PsWeb.ProfileLive.Show do
 
   @impl true
   def handle_params(%{"username" => username}, _, socket) do
+    profile = Profiles.get_profile_by_username!(username)
+
     {:noreply,
      socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:profile, Profiles.get_profile_by_username!(username))}
+     |> assign(:profile, profile)
+     |> assign(:username, username)
+     |> assign(:page_title, page_title(socket.assigns.live_action, profile))}
   end
 
-  defp page_title(:show), do: "Show Profile"
-  defp page_title(:edit), do: "Edit Profile"
+  defp page_title(:show, profile) do
+    case profile do
+      nil -> "Profile not found"
+      _ -> "#{profile.name} (@#{profile.username})"
+    end
+  end
+
+  defp page_title(:edit, profile) do
+    case profile do
+      nil -> "Profile not found"
+      _ -> "Edit #{profile.name} (@#{profile.username})"
+    end
+  end
 end
