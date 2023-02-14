db01e7601c00977f6ac22a2650d03660f44d6efc
Zen <ztang230@gmail.com>
2023-02-14 12:00:49 -0600

    [profile] temp page

--- lib/ps_web/controllers/page_controller.ex
+++ lib/ps_web/controllers/page_controller.ex
@@ -15,19 +15,8 @@ defmodule PsWeb.PageController do
   end
 
   def plain_html(conn, %{"page" => page}) do
-    profile2 = %{
-      name: "Jane Doe",
-      byline:
-        "(If it was easy, everyone would do it) Greetings, peoples. Welcome to the blog of all the fun things. Here be comics, Pratchett, vaguely-Middle-Ages-related things, Avengers, angry feminism, all the book-love and a chocolate biscuit. Now proud owner of a one starship fleet known as My Starship Now
-      Enjoy.",
-      bio:
-        "I sit at the intersection of design, anthropology, and programming. These three are at the core of everything I make. Combining them into a coherent career is a weird and ongoing challenge.
-        ",
-      avatar: "https://avatars.githubusercontent.com/u/30219253?v=4"
-    }
-
     render(conn, "#{page}.html",
-      profile: profile2,
+      profile: Ps.Profiles.get_profile!(12),
       layout: false,
       show_header: false,
       public_posts: Ps.Posts.list_public_posts(),
